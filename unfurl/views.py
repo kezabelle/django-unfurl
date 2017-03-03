5cc50fa803794716b16e0480b2775785949fc630
Keryn Knight <keryn@kerynknight.com>
2017-03-03 20:25:14 +0000

    Add server-side search.

--- unfurl/views.py
+++ unfurl/views.py
@@ -2,13 +2,42 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.conf import settings
+from django.forms import CharField
+from django.forms import Form
 from django.template.response import TemplateResponse
 
 from unfurl.extractor import get_urls
 
 
+class SearchForm(Form):
+    text = CharField()
+
+    def __init__(self, urls, *args, **kwargs):
+        super(SearchForm, self).__init__(*args, **kwargs)
+        self.urls = (urls)
+
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        parts = text.split(" ")
+        return tuple(part for part in parts if part.strip() != '')
+
+    def filter(self):
+        if self.is_valid():
+            for url in self.urls:
+                for user_filter in self.cleaned_data['text']:
+                    # look for character or partial or full match somewhere in the
+                    # string of the set()
+                    if user_filter in url['textsearch']:
+                        yield url
+        else:
+            for url in self.urls:
+                yield url
+
+
 def urls_list(request):
-    urls = get_urls(settings.ROOT_URLCONF)
+    all_urls = get_urls(settings.ROOT_URLCONF)
+    form = SearchForm(data=request.GET or None, initial=None, urls=all_urls)
+    urls = form.filter()
     viewname_template = request.resolver_match.view_name.replace(':', '/')
     template = (
         '{}.html'.format(viewname_template),
@@ -16,5 +45,6 @@ def urls_list(request):
     )
     context = {
         'urlconf': urls,
+        'form': form,
     }
     return TemplateResponse(request=request, template=template, context=context)
