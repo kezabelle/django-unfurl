98a70ee76160f64f927e13f17fcd6ef0ee18a5d5
Keryn Knight <keryn@kerynknight.com>
2017-03-04 11:31:44 +0000

    Fix tests for 1.8, which didn't have extra redirect views the admin See build failure https://travis-ci.org/kezabelle/django-unfurl/builds/207518030

--- unfurl/tests/test_views.py
+++ unfurl/tests/test_views.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+
+import django
+
 from unfurl.views import urls_list
 
 
@@ -7,11 +10,19 @@ def test_view_load_all(rf):
     request = rf.get('/')
     response = urls_list(request)
     urls = tuple(response.context_data['urlconf'])
-    assert len(urls) == 36
+    if django.VERSION[0:2] > (1, 8):
+        assert len(urls) == 36
+    else:
+        # newer Djangos have redirect views in the admin
+        assert len(urls) == 33
 
 
 def test_view_load_some(rf):
     request = rf.get('/', data={'text': 'auth'})
     response = urls_list(request)
     urls = tuple(response.context_data['urlconf'])
-    assert len(urls) == 21
+    if django.VERSION[0:2] > (1, 8):
+        assert len(urls) == 21
+    else:
+        # newer Djangos have redirect views in the admin
+        assert len(urls) == 19
